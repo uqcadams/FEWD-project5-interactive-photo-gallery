b1a1b0fd4064920c09a426110cf39ca031b9ac9b
Chris Adams <uqcadams@gmail.com>
2021-03-14 18:15:13 +1000

    Finished Project 5
    Updated styles and interactive elements

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
+
 function search_gallery() { 
     let input = document.getElementById('searchbar').value 
     input=input.toLowerCase(); 
-    // let x = document.getElementsByClassName('galleryImage'); 
     let x = document.getElementsByClassName('galleryImage'); 
       
     for (i = 0; i < x.length; i++) {  
